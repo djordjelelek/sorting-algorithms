e238934fbc23efafd1f0b3ed54b31bcf22f103ed
Djordje Lelek <djordje.lelek@gmail.com>
2021-03-09 13:09:37 +0100

    Add checker at bubble sort

--- 2. BubbleSort.js
+++ 2. BubbleSort.js
@@ -1,13 +1,15 @@
 const bubbleSort = (arr) => {
-  const length = arr.length;
-  for (let i = 0; i < length - 1; i++) {
-    for (let j = 0; j < length - 1 - i; j++) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let checker = true;
+    for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        checker = false;
       }
     }
+    if (checker) break;
   }
   return arr;
 };
